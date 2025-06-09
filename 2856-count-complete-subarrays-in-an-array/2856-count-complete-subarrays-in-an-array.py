bbfac6c4d6c2c87b23d92a1e5d25c9f045c6d687
Kthesword <155190425+Kthesword@users.noreply.github.com>
2025-06-09 21:54:45 +0300

    Time: 583 ms (10.79%), Space: 18.1 MB (29.07%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        total_unique = len(set(nums))
+        n = len(nums)
+        res = 0
+
+        for left in range(n):
+            seen = set()
+            for right in range(left, n):
+                seen.add(nums[right])
+                if len(seen) == total_unique:
+                    res += 1  
+        return res
