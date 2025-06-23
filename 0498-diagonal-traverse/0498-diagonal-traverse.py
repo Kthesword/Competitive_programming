ef47d7d1e63b668d1e9ec1da79bc4661fbabc3cc
Kthesword <155190425+Kthesword@users.noreply.github.com>
2025-06-23 23:05:06 +0300

    Time: 11 ms (64.82%), Space: 19.6 MB (96.98%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,41 +1,31 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        rows = len(mat)
-        cols = len(mat[0])
-        i, j = 0, 0
-        arr = []
-        while len(arr) < rows*cols:
-            while i >= 0 and j < cols:
-                arr.append(mat[i][j])
-               
-                i -= 1
-                j += 1
-            i += 1
-            j -= 1
-
-            if i == rows-1 and j == cols-1:
-                break
-            
-            if j + 1 < cols:
-                j += 1
+        m = len(mat)
+        n = len(mat[0])
+        i = 0
+        ans = []
+        dirc = 'up'
+        x,y = 0, 0
+        while i < m + n - 1:
+            if 0 <= x < m and 0 <= y < n:
+                ans.append(mat[x][y])
+    
+            if dirc == 'up':
+                if y+1 < n and x-1 >= 0:
+                    x -= 1
+                    y += 1
+                else:
+                    dirc = 'down'
+                    y += 1
+                    i += 1
             else:
-                i += 1
-                  
-            while i < rows and j >= 0:
-                curr = mat[i][j]
-                arr.append(curr)
-                i += 1
-                j -= 1
-            i -= 1
-            j += 1
+                if x+1 < m and y-1 >= 0:
+                    x += 1
+                    y -= 1
+                else:
+                    dirc = 'up'
+                    x += 1
+                    i += 1
 
-            if i == rows-1 and j == cols-1:
-                break
-
-            if i + 1 < rows:
-                i += 1
-            else:
-                j += 1
-
-        return arr
+        return ans
                 
